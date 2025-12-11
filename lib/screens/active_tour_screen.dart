54fc5138fefe3c1010600a1f8e241cd6ce98dad8
Dilara <dilara.schulze@gmail.com>
2025-12-11 11:43:59 +0100

    fixed color value

--- lib/screens/active_tour_screen.dart
+++ lib/screens/active_tour_screen.dart
@@ -69,7 +69,7 @@ class _ActiveTourScreenState extends State<ActiveTourScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Active: ${widget.tour.title}'),
-        backgroundColor: const Color(0xFFFFFF),
+        backgroundColor: const Color(0xFFFFFFFF),
         actions: [
           IconButton(
             icon: const Icon(Icons.settings),
